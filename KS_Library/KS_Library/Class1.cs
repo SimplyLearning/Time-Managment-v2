ab960957c45127c276a626f214a997c4758d404d
Keagan Simpson <101067031+SimplyLearning@users.noreply.github.com>
2023-01-14 22:48:53 +0200

    Add files via upload
    This is the external class library that references this application. IMPORTANT

---
+++ KS_Library/KS_Library/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KS_Library
+{
+    public class modulesLibrary
+    {
+        public string code { get; set; }
+        public string modules { get; set; }
+        public string name { get; set; }
+        public int numCredit { get; set; }
+        public int hoursWeek { get; set; }
+        public int numWeeks { get; set; }
+        public DateTime startDate { get; set; }
+        public double ssHours { get; set; }
+        public int numHours { get; set; }
+
+        //storing num of hours on certain date 
+        public double hourSpent { get; set; }
+        public DateTime certainDate { get; set; }
+        public String moduleStudied { get; set; }
+
+        //this list is to store the module info 
+
+        // public List<modulesLibrary> modList = new List<modulesLibrary>();
+       public  Dictionary<String, double> modHours = new Dictionary<string, double>();
+
+
+        //creating a list  to store the Self-Study Hours 
+        //public List<double> hoursList = new List<double>();
+            
+
+        public double Calculation(int Credits, int hourw, int numw)
+        {
+            // calculation for self study hours
+            return ssHours = ((Credits * 10) / numw) - hourw;
+        }
+
+    }
+    
+}
